23dd67a1e179ff701926b9745381d78f932e6c93
Phil Henrickson <=>
2024-07-31 11:43:18 -0500

    adding some espn tables

--- _targets.R
+++ _targets.R
@@ -22,97 +22,172 @@ tar_source("R")
 # tar_source("other_functions.R") # Source other scripts as needed.
 
 # static branching over seasons
-seasons = data.frame(season = 2000:2023)
+seasons = 2000:2023
 
 # Replace the target list below with your own:
-cfbd = 
-    list(
-        ### cfbd data
-        # calendars
-        tar_target(
-            cfbd_calendars_tbl,
-            map_df(seasons$season,
-                   ~ cfbd_calendar(year = .x)) |>
+list(
+    ### cfbd data
+    # calendars
+    tar_target(
+        cfbd_calendars_tbl,
+        map_df(seasons,
+               ~ cfbd_calendar(year = .x)) |>
+            as_tibble()
+    ),
+    # conferences
+    tar_target(
+        cfbd_conferences_tbl,
+        cfbd_conferences() |>
+            as_tibble()
+    ),
+    # fbs team info
+    tar_target(
+        cfbd_team_info_tbl,
+        map_df(seasons,
+               ~ cfbd_team_info(only_fbs = T,
+                                year = .x) |>
+                   as_tibble() |>
+                   mutate(season = .x)) |>
+            select(season, everything())
+    ),
+    # talent
+    tar_target(
+        cfbd_team_talent_tbl,
+        map_df(seasons,
+               ~ cfbd_team_talent(year = .x)) |>
+            as_tibble()
+    ),
+    # team recruiting,
+    tar_target(
+        cfbd_recruiting_team,
+        map_df(seasons,
+               ~ cfbd_recruiting_team(year = .x)) |>
+            as_tibble()
+    ),
+    # games
+    tar_target(
+        cfbd_game_info_tbl,
+        map_df(seasons,
+               ~ cfbd_game_info(year = .x,
+                                season_type = "both")) |>
+            as_tibble()
+    ),
+    # betting lines
+    tar_target(
+        cfbd_betting_lines_tbl,
+        {
+            tmp = expand_grid(season = seasons, 
+                              type = c("regular", "postseason"))
+            
+            map2_df(.x = tmp$season,
+                    .y = tmp$type,
+                    ~ cfbd_betting_lines(year = .x,
+                                         season_type = .y)
+            )
+        }
+    ),
+    # rankings
+    tar_target(
+        cfbd_game_rankings_tbl,
+        {
+            tmp = expand_grid(season = seasons, 
+                              type = c("regular", "postseason"))
+            
+            map2_df(.x = tmp$season,
+                    .y = tmp$type,
+                    ~ cfbd_rankings(year = .x,
+                                    season_type = .y)
+            )
+        }
+    ),
+    # draft picks
+    tar_target(
+        cfbd_draft_picks,
+        map_df(seasons,
+               ~ cfbd_draft_picks(year = .x)) |>
+            as_tibble()
+    ),
+    # play types
+    tar_target(
+        cfbd_play_types_tbl,
+        cfbd_play_types() |>
+            as_tibble()
+    ),
+    # coaches
+    tar_target(
+        cfbd_coaches_tbl,
+        map_df(seasons,
+               ~ cfbd_coaches(year = .x) |>
+                   add_season(year = .x))
+    ),
+    # rosters
+    tar_target(
+        cfbd_team_roster_tbl,
+        map_df(seasons,
+               ~  cfbd_team_roster(year = .x) |>
+                   add_season(year = .x))
+    ),
+    # recruiting_player
+    tar_target(
+        cfbd_recruiting_player_tbl,
+        map_df(seasons,
+               ~ cfbd_recruiting_player(year = .x) |>
+                   add_season(year = .x))
+    ),
+    # recruiting position
+    tar_target(
+        cfbd_recruiting_position_tbl,
+        map_df(seasons,
+               ~ cfbd_recruiting_position(start_year = .x,
+                                          end_year = .x) |>
+                   add_season(year = .x))
+    ),
+    # drives
+    tar_target(
+        cfbd_drives_tbl,
+        map_df(seasons,
+               ~ cfbd_drives(year = .x, 
+                             season_type = 'both') |>
+                   add_season(year = .x))
+    ),
+    # plays
+    tar_target(
+        cfbd_plays_tbl,
+        map_df(seasons,
+               ~ cfbd_plays(year = .x, 
+                            season_type = 'both') |>
+                   add_season(year = .x))
+    ),
+    ### now get espn data
+    # calendar
+    tar_target(
+        espn_cfb_calendar_tbl,
+        map_df(
+            seasons,
+            ~ espn_cfb_calendar(year = .x) |>
                 as_tibble()
-        ),
-        # conferences
-        tar_target(
-            cfbd_conferences_tbl,
-            cfbd_conferences() |>
-                as_tibble()
-        ),
-        # fbs team info
-        tar_target(
-            cfbd_team_info_tbl,
-            map_df(seasons$season,
-                   ~ cfbd_team_info(only_fbs = T,
-                                    year = .x) |>
-                       as_tibble() |>
-                       mutate(season = .x)) |>
-                select(season, everything())
-        ),
-        # talent
-        tar_target(
-            cfbd_team_talent_tbl,
-            map_df(seasons$season,
-                   ~ cfbd_team_talent(year = .x)) |>
-                as_tibble()
-        ),
-        # team recruiting,
-        tar_target(
-            cfbd_recruiting_team,
-            map_df(seasons$season,
-                   ~ cfbd_recruiting_team(year = .x)) |>
-                as_tibble()
-        ),
-        # games
-        tar_target(
-            cfbd_game_info_tbl,
-            map_df(seasons$season,
-                   ~ cfbd_game_info(year = .x,
-                                    season_type = "both")) |>
-                as_tibble()
-        ),
-        # betting lines
-        tar_target(
-            cfbd_betting_lines_tbl,
-            {
-                tmp = expand_grid(season = seasons$season, 
-                                  type = c("regular", "postseason"))
-                
-                map2_df(.x = tmp$season,
-                        .y = tmp$type,
-                        ~ cfbd_betting_lines(year = .x,
-                                             season_type = .y)
-                )
-            }
-        ),
-        # rankings
-        tar_target(
-            cfbd_game_rankings_tbl,
-            {
-                tmp = expand_grid(season = seasons$season, 
-                                  type = c("regular", "postseason"))
-                
-                map2_df(.x = tmp$season,
-                        .y = tmp$type,
-                        ~ cfbd_rankings(year = .x,
-                                        season_type = .y)
-                )
-            }
-        ),
-        # draft picks
-        tar_target(
-            cfbd_draft_picks,
-            map_df(seasons$season,
-                   ~ cfbd_draft_picks(year = .x))
-        ),
-        # play types
-        tar_target(
-            cfbd_play_types_tbl,
-            cfbd_play_types()
         )
+    ),
+    # schedule
+    tar_target(
+        espn_cfb_schedule_tbl,
+        map_df(
+            seasons,
+            ~ espn_cfb_schedule(year = .x) |>
+                as_tibble()
+        )
+    ),
+    # espn games
+    tar_target(
+        espn_cfb_game_ids,
+        espn_cfb_schedule_tbl |> 
+            # seasons with pbp data
+            filter(season > 2002) |>
+            filter(play_by_play_available == T) |>
+            distinct(game_id) |>
+            pull()
     )
+)
 
 # # dynamic branch over games, weeks, seasons
 # dynamic = 
@@ -133,77 +208,77 @@ cfbd =
 #         )
 #     )
 
-# static branch over seasons
-static =
-    # static branch over seasons
-    tar_map(
-        values = seasons,
-        # drives
-        tar_target(
-            cfbd_drives_by_season,
-            cfbd_drives(year = season,
-                        season_type = 'both') |>
-                add_season(year = season)
-        ),
-        # plays
-        tar_target(
-            cfbd_plays_by_season,
-            cfbd_plays(season_type = 'both') |>
-                add_season(year = season) 
-        ),
-        # coaches
-        tar_target(
-            cfbd_coaches_by_season,
-            cfbd_coaches(year = season) |>
-                add_season(year = season)
-        ),
-        # rosters
-        tar_target(
-            cfbd_team_roster_by_season,
-            cfbd_team_roster(year = season) |>
-                add_season(year = season)
-        ),
-        # recruits
-        tar_target(
-            cfbd_recruiting_player_by_season,
-            cfbd_recruiting_player(year = season) |>
-                add_season(year = season)
-        )
-    )
-
-# combine results from static branches
-combined = 
-    list(
-        tar_combine(
-            cfbd_drives_tbl,
-            static[["cfbd_drives_by_season"]],
-            command = dplyr::bind_rows(!!!.x)
-        ),
-        tar_combine(
-            cfbd_plays_tbl,
-            static[["cfbd_plays_by_season"]],
-            command = dplyr::bind_rows(!!!.x)
-        ),
-        tar_combine(
-            cfbd_coaches_tbl,
-            static[["cfbd_coaches_by_season"]],
-            command = dplyr::bind_rows(!!!.x)
-        ),
-        tar_combine(
-            cfbd_team_roster_tbl,
-            static[["cfbd_team_roster_by_season"]],
-            command = dplyr::bind_rows(!!!.x)
-        ),
-        tar_combine(
-            cfbd_recruiting_player_tbl,
-            static[["cfbd_recruiting_player_by_season"]],
-            command = dplyr::bind_rows(!!!.x)
-        )
-    )
-
-# return results
-list(
-    cfbd,
-    static,
-    combined
-)
+# # static branch over seasons
+# static =
+#     # static branch over seasons
+#     tar_map(
+#         values = seasons,
+#         # drives
+#         tar_target(
+#             cfbd_drives_by_season,
+#             cfbd_drives(year = season,
+#                         season_type = 'both') |>
+#                 add_season(year = season)
+#         ),
+#         # plays
+#         tar_target(
+#             cfbd_plays_by_season,
+#             cfbd_plays(season_type = 'both') |>
+#                 add_season(year = season) 
+#         ),
+#         # coaches
+#         tar_target(
+#             cfbd_coaches_by_season,
+#             cfbd_coaches(year = season) |>
+#                 add_season(year = season)
+#         ),
+#         # rosters
+#         tar_target(
+#             cfbd_team_roster_by_season,
+#             cfbd_team_roster(year = season) |>
+#                 add_season(year = season)
+#         ),
+#         # recruits
+#         tar_target(
+#             cfbd_recruiting_player_by_season,
+#             cfbd_recruiting_player(year = season) |>
+#                 add_season(year = season)
+#         )
+#     )
+# 
+# # combine results from static branches
+# combined = 
+#     list(
+#         tar_combine(
+#             cfbd_drives_tbl,
+#             static[["cfbd_drives_by_season"]],
+#             command = dplyr::bind_rows(!!!.x)
+#         ),
+#         tar_combine(
+#             cfbd_plays_tbl,
+#             static[["cfbd_plays_by_season"]],
+#             command = dplyr::bind_rows(!!!.x)
+#         ),
+#         tar_combine(
+#             cfbd_coaches_tbl,
+#             static[["cfbd_coaches_by_season"]],
+#             command = dplyr::bind_rows(!!!.x)
+#         ),
+#         tar_combine(
+#             cfbd_team_roster_tbl,
+#             static[["cfbd_team_roster_by_season"]],
+#             command = dplyr::bind_rows(!!!.x)
+#         ),
+#         tar_combine(
+#             cfbd_recruiting_player_tbl,
+#             static[["cfbd_recruiting_player_by_season"]],
+#             command = dplyr::bind_rows(!!!.x)
+#         )
+#     )
+# 
+# # return results
+# list(
+#     cfbd,
+#     static,
+#     combined
+# )
